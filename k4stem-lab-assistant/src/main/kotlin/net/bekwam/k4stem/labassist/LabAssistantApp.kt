7ae72b0f943f1bb86d0b26a8a9580c4a15dda551
Robert <robert@bekwam.com>
2018-06-12 08:02:58 -0400

    Resolved issue #3

--- k4stem-lab-assistant/src/main/kotlin/net/bekwam/k4stem/labassist/LabAssistantApp.kt
+++ k4stem-lab-assistant/src/main/kotlin/net/bekwam/k4stem/labassist/LabAssistantApp.kt
@@ -1,7 +1,11 @@
 package net.bekwam.k4stem.labassist
 
 import javafx.scene.Scene
+import javafx.scene.web.WebEngine
+import javafx.stage.FileChooser
+import javafx.stage.Stage
 import tornadofx.*
+import java.io.File
 
 /**************************************************
 
@@ -10,12 +14,62 @@ import tornadofx.*
 **************************************************/
 
 class MainView : View("K4Stem Lab Assistant") {
-    override val root = vbox { label("Content goes here")}
+    override val root = borderpane {
+        label("Content goes here")
+        top = menubar {
+                menu("File") {
+                    item("New")
+                    item("Open") {
+                    action {
+                        val filterFiles = arrayOf(FileChooser.ExtensionFilter("Files", "*.txt",".*kt"))
+                        val file = chooseFile("Select File", filterFiles, FileChooserMode.Single)
+                        fun fileNamer(file: File?): String {
+                            if (file != null) {
+                                return "K4Stem Lab Assistant - $file"
+                            } else {
+                                return "K4Stem Lab Assistant"
+                            }
+                        }
+                        title = "${fileNamer(file[0])}"
+                    }
+                }
+                    menu("Open Recent") {}
+                    item("Close")
+                    separator()
+                    item("Save")
+                    item("Save As")
+                    separator()
+                    item("Preferences")
+                    separator()
+                    item("Exit")
+                }
+                menu("Edit") {
+                    item("Cut")
+                    item("Copy")
+                    item("Paste")
+                }
+                menu("View")
+                menu("Help") {
+                    item("Wiki") {
+                        action {
+                            hostServices.showDocument("https://github.com/bekwam/k4stem/wiki")
+                        }
+                    }
+                    item("About")
+                }
+        }
+    }
 }
+
+
+
+
+
 
 class LabAssistantApp : App(MainView::class) {
     override fun createPrimaryScene(view: UIComponent) = Scene(view.root, 800.0, 600.0)
 }
+
 
 fun main(args : Array<String>) {
     launch<LabAssistantApp>(args)

4a5d1a921298bb11428fff7370fc3d5f802c8aa1
walkerca <carl@bekwam.com>
2018-06-13 08:51:36 -0400

    [#8] Updated pom to include Klaxon
    Some test data files

---
+++ k4stem-lab-assistant/src/test/kotlin/net/bekwam/k4stem/labassist/ParseSimpleFile.kt
@@ -0,0 +1,10 @@
+package net.bekwam.k4stem.labassist
+
+fun main(args : Array<String>) {
+
+    // create a person object based on the contents of ParseSimpleFile.kt
+
+    // first, hardcode a person string and pass that in as in the klaxon example
+
+    // next, read in the file input and use that as the argument to klaxon
+}
